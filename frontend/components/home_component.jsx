696d2ec41195fa20ec1cd9a1caf4fda0a6536b32
Nick Vizzutti <njvizzutti@gmail.com>
2017-01-12 00:22:32 -0800

    added homepage footer

--- frontend/components/home_component.jsx
+++ frontend/components/home_component.jsx
@@ -6,11 +6,21 @@ const tagLines = ["Get started, let's watch the game",
 "You know the players, now know the fanbase",
 "Other fans are just friends you haven't watched games with"];
 let currentTag = tagLines[Math.floor(Math.random()*tagLines.length)];
+const footer = () => (
+  <div className='home-footer'>
+    <h1 className='col-1'>Meetup with fellow fans to watch game</h1>
+    <h2 className='col-1'>You some others meet a host at a venue</h2>
+    <h1 className='col-1'>That's it! Sporting events bring people together</h1>
+  </div>
+);
 
 const HomeComponent = () => (
+  <section className='home-main'>
   <div className="home-wrapper">
     <Link to='cities'>{currentTag}</Link>
   </div>
+    {footer()}
+  </section>
 );
 
 export default HomeComponent;
