a9dff0470de0fd373bb2dc38b864815765d91ad1
Nick Vizzutti <njvizzutti@gmail.com>
2017-01-13 22:06:20 -0800

    city event list api call

--- frontend/components/city_show_container.jsx
+++ frontend/components/city_show_container.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import CityShow from './city_show';
-import {fetchCurrentCity} from '../actions/city_actions';
+import {fetchCurrentCity, fetchCities} from '../actions/city_actions';
 import {fetchCityEvents} from '../actions/event_actions';
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    city_id: ownProps.params.city_id,
     city: state.cities.currentCity,
     events: state.events
   };
@@ -17,4 +16,5 @@ const mapDispatchToProps = dispatch => ({
   fetchCityEvents: id => dispatch(fetchCityEvents(id))
 });
 
+window.fetchCurrentCity = fetchCurrentCity;
 export default connect(mapStateToProps, mapDispatchToProps)(CityShow);
