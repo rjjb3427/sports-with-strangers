45e51cfd4b32cb1bde87d95e98785cbc6d3e3543
Nick Vizzutti <njvizzutti@gmail.com>
2017-03-10 12:29:36 -0800

    altered css

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ActiveRecord::Base
   validates :city, :host, :title, :address, :sport, :time, presence: true
+  validates :title, length: { minimum: 4, maximum: 40}
+  validates :address, length: {maximum: 40}
+  validate :capacity_range
+  validate :valid_address
   validates :title, uniqueness: true
 
   belongs_to :host,
@@ -12,4 +16,18 @@ class Event < ActiveRecord::Base
   has_many :attending_users,
   through: :attendees,
   source: :user
+  def capacity_range
+    capacity = self.capacity
+    errors.add(:capacity, "cannot be zero") unless capacity > 0
+    errors.add(:capacity, "is too large") if capacity > 20
+  end
+
+  def valid_address
+    address = self.address
+    nums = address.scan(/\d+/)
+    chars = address.scan(/\w+/)
+    if nums.length.zero? || chars.length.zero?
+      errors.add(:address, "must be valid")
+    end 
+  end
 end
